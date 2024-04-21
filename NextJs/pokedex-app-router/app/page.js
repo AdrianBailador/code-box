c7477ca35d2954efb8491e3caa0f09c21de68390
Victor Alvarado <vito8916@gmail.com>
2024-04-21 19:39:55 +0100

    bug/pokedex-app-router/add correct css class

--- NextJs/pokedex-app-router/app/page.js
+++ NextJs/pokedex-app-router/app/page.js
@@ -1,10 +1,11 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import PokeList from "@/components/PokeList";
+import HomeCss from "../styles/Home.module.css";
 
 export default function Home() {
   return (
-    <main className={styles.main}>
+    <main className={HomeCss.container}>
       <h1>PokeDex</h1>
       <PokeList />
     </main>
