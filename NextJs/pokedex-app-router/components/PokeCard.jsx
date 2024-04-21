89bf402015668bce9996f32ce544f94b01f85c33
Victor Alvarado <vito8916@gmail.com>
2024-04-21 18:12:11 +0100

    feat/added components

---
+++ NextJs/pokedex-app-router/components/PokeCard.jsx
@@ -0,0 +1,40 @@
+import Link from 'next/link';
+import HomeCss from '../styles/Home.module.css' // Importa el módulo CSS para el estilo
+
+const PokeCard = ({pokemon}) => {
+    return (
+        // Un componente Link para la ruta a la página del pokemon
+        <Link scroll={false} href={{
+            pathname: '/pokemon/[name]',
+            query: {name: pokemon.name}
+        }}>
+            <div className={`${HomeCss.card} ${pokemon.types[0].type.name}`}>
+                <div className={HomeCss.nombreTipos}>
+
+                    <h3 exit={{opacity: 0}}>{pokemon.name}</h3>
+
+
+                    <div className={HomeCss.tipos}>
+                        {pokemon.types.map((tipos, index) => { // Mapea los tipos del pokemon
+                            return (
+                                <div key={index} className={HomeCss.tipo}>
+                                    {tipos.type.name}
+                                </div>
+                            )
+                        })}
+                    </div>
+                </div>
+                <img
+                    src={pokemon.sprites} // La URL de la imagen del pokemon
+                    alt={pokemon.name} // El nombre del pokemon como texto alternativo para la imagen
+                    width={100}
+                    height={100}
+                    className={HomeCss.imagen} // La clase CSS para la imagen
+                />
+            </div>
+        </Link>
+
+    );
+};
+
+export default PokeCard;
---
+++ NextJs/pokedex-app-router/components/PokeList.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import PokeCard from "@/components/PokeCard";
+import HomeCss from '../styles/Home.module.css' // Importa el módulo CSS para el estilo
+
+
+const PokeList = async ({pokemon}) => {
+    // Hace una petición a la API de Pokemon
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=10&offset=0.`)
+    // Convierte la respuesta en JSON
+    const data = await response.json()
+    const pokemonList = data.results
+
+    console.log(pokemonList)
+
+    return (
+        <div className={HomeCss.columnas}>
+            <ul>
+                {
+                    pokemonList.map((pokemon) => (
+                    <li key={pokemon.id}>
+                        <PokeCard pokemon={pokemon}/>
+                    </li>
+                    ))
+                }
+            </ul>
+        </div>
+    );
+};
+
+export default PokeList;
