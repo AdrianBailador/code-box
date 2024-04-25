ab285de648909ac3785cd0b2d08c48384f758e57
Victor Alvarado <vito8916@gmail.com>
2024-04-25 23:33:13 +0100

    refactor/pokedex-app-router/refactor the main page of the project.

--- NextJs/pokedex-app-router/app/page.js
+++ NextJs/pokedex-app-router/app/page.js
@@ -1,13 +1,10 @@
-import Image from "next/image";
-import styles from "./page.module.css";
 import PokeList from "@/components/PokeList";
-import HomeCss from "../styles/Home.module.css";
 
 export default function Home() {
-  return (
-    <main className={HomeCss.container}>
-      <h1>PokeDex</h1>
-      <PokeList />
-    </main>
-  );
+    return (
+        <main className='wrapper'>
+            <h1 className='logo-pokemon'>Pokédex</h1>
+            <PokeList/>
+        </main>
+    );
 }
