5d29e57a6c9e614d97bb6157194052c817e72659
Victor Alvarado <vito8916@gmail.com>
2024-04-26 02:02:57 +0100

    feat/pokedex-app-router/file to wrap the API of pokeAPI

---
+++ NextJs/pokedex-app-router/utils/pokeAPIWrapper.js
@@ -0,0 +1,16 @@
+const pokeAPI = 'https://pokeapi.co/api/v2/';
+
+//function to get the list of pokemons
+export async function getPokemonList() {
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=27&offset=0`)
+    const data = await response.json()
+    return data.results
+}
+
+//function to get the pokemon by name
+export async function getPokemonByName(name) {
+    const response = await fetch(`${pokeAPI}pokemon/${name}`);
+    const data = await response.json();
+
+    return data;
+}
