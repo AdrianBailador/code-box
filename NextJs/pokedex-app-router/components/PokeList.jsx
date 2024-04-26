16d663bd4cdcd20cc30f02c9d7038d48446a8bbb
Victor Alvarado <vito8916@gmail.com>
2024-04-26 02:05:18 +0100

    refactor/pokedex-app-router/change the way fetch the pokemon data. Now using the API wrapper

--- NextJs/pokedex-app-router/components/PokeList.jsx
+++ NextJs/pokedex-app-router/components/PokeList.jsx
@@ -1,11 +1,9 @@
 import PokeCard from "@/components/PokeCard";
+import {getPokemonList} from "@/utils/pokeAPIWrapper";
 
 const PokeList = async () => {
-    // Hace una petición a la API de Pokemon
-    const response = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=27&offset=0.`)
-    // Convierte la respuesta en JSON
-    const data = await response.json()
-    const pokemonList = data.results
+
+    const pokemonList = await getPokemonList()
 
     const allPokemonData = await Promise.all(pokemonList.map(async (pokemon) => {
         const response = await fetch(pokemon.url)
