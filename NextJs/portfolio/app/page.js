a022e8220b645557117f1c8b79a3d888141b488d
Victor Alvarado <vito8916@gmail.com>
2024-04-26 20:04:16 +0100

    feat/portfolio/add last changes

--- NextJs/portfolio/app/page.js
+++ NextJs/portfolio/app/page.js
@@ -3,7 +3,7 @@ import {Button} from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="">
       <h1>Portfolio</h1>
         <Button variant={"ghost"}>
             Soy unn Boton
