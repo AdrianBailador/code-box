a9aa0e4f83185b14016542d2c7086d0b0ed92d4c
Victor Alvarado <vito8916@gmail.com>
2024-04-30 20:24:35 +0100

    refactor/portfolio/delete unnecessary libs

--- NextJs/portfolio/app/page.js
+++ NextJs/portfolio/app/page.js
@@ -1,7 +1,4 @@
-import Image from "next/image";
-import {Button} from "@/components/ui/button";
 import ProfileInfo from "@/components/ProfileInfo";
-import SocialIcons from "@/components/SocialIcons";
 import ProfileActionButtons from "@/components/ProfileActionButtons";
 
 export default function Home() {
