5afc1e96bdcd56cf863a34a148e913955403231f
Victor Alvarado <vito8916@gmail.com>
2024-04-30 19:52:23 +0100

    feat/portfolio/add compoennt

--- NextJs/portfolio/app/page.js
+++ NextJs/portfolio/app/page.js
@@ -2,11 +2,13 @@ import Image from "next/image";
 import {Button} from "@/components/ui/button";
 import ProfileInfo from "@/components/ProfileInfo";
 import SocialIcons from "@/components/SocialIcons";
+import ProfileActionButtons from "@/components/ProfileActionButtons";
 
 export default function Home() {
-  return (
-    <main className="mt-[2rem]">
-        <ProfileInfo/>
-    </main>
-  );
+    return (
+        <main className="mt-[2rem] flex flex-col gap-4 justify-start items-center">
+            <ProfileInfo/>
+            <ProfileActionButtons/>
+        </main>
+    );
 }
