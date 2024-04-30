599f8cef87247d3367514d5fb9604f985d6e618b
Victor Alvarado <vito8916@gmail.com>
2024-04-30 19:51:56 +0100

    refactor/portfolio/add styles

--- NextJs/portfolio/components/ProfileInfo.jsx
+++ NextJs/portfolio/components/ProfileInfo.jsx
@@ -1,6 +1,7 @@
 // ProfileInfo.jsx
 import SocialIcons from './SocialIcons';
 import ProfileImage from "@/components/ProfileImage";
+import ProfileActionButtons from "@/components/ProfileActionButtons";
 
 
 const ProfileInfo = () => {
@@ -10,7 +11,7 @@ const ProfileInfo = () => {
         <div style={{ borderRadius: '50%', overflow: 'hidden', marginRight: '1rem' }}>
            <ProfileImage url={'/foto.png'} title={'My profile picture'} width={'100'} height={'100'} />
         </div>
-        <div className={'flex flex-col gap-2'}>
+        <div className={'profile-info flex flex-col gap-2'}>
           <div className="info">
             <h1 className={'text-2xl text-gray-700 font-bold'}>Victor and Adri</h1>
             <p className={'italic'}>FullStack Developer</p>
