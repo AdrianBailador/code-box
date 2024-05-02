22f3a7f6f68896bac32ce892af32c95f895a8e4a
Victor Alvarado <vito8916@gmail.com>
2024-05-02 18:57:07 +0100

    refactor/portfolio/delete social array and move to the static content file

--- NextJs/portfolio/components/SocialIcons.jsx
+++ NextJs/portfolio/components/SocialIcons.jsx
@@ -1,34 +1,11 @@
-import {FaInstagram, FaTwitter, FaYoutube, FaGithub, FaTiktok} from 'react-icons/fa';
 import Link from 'next/link';
-
-const socialnetworks = [
-    {
-        name: 'Instagram',
-        url: 'https://www.instagram.com/tu_usuario',
-        icon: <FaInstagram size={24}/>,
-    },
-    {
-        name: 'Twitter',
-        url: 'https://twitter.com/tu_usuario',
-        icon: <FaTwitter size={24}/>,
-    },
-    {
-        name: 'YouTube',
-        url: 'https://www.youtube.com/c/tu_usuario',
-        icon: <FaYoutube size={24}/>,
-    },
-    {
-        name: 'GitHub',
-        url: 'http://github.com',
-        icon: <FaGithub size={24}/>
-    },
-]
+import {socialNetworks} from '../static_content/index';
 
 const SocialIcons = () => {
     return (
         <div style={{display: 'flex', gap: '1rem'}}>
             {
-                socialnetworks.map((networkS) => (
+                socialNetworks.map((networkS) => (
                     <Link
                         className={networkS.name}
                         href={networkS.url} key={networkS.name}
