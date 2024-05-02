702e67cbb2e64fa06978dac103bd14f15347fe71
Victor Alvarado <vito8916@gmail.com>
2024-05-02 18:55:30 +0100

    feat/portfolio/add hamburger menu

---
+++ NextJs/portfolio/assets/MenuSvg.jsx
@@ -0,0 +1,34 @@
+'use client';
+const MenuSvg = ({ openNavigation }) => {
+    return (
+        <svg
+            className="overflow-visible"
+            width="20"
+            height="20"
+            viewBox="0 0 20 12"
+        >
+            <rect
+                style={{ transformOrigin: 'center'}}
+                className={`transition-all origin-center`}
+                y={openNavigation ? "5" : "0"}
+                width="20"
+                height="2"
+                rx="1"
+                fill="white"
+                transform={`rotate(${openNavigation ? "45" : "0"})`}
+            />
+            <rect
+                style={{ transformOrigin: 'center'}}
+                className="transition-all origin-center"
+                y={openNavigation ? "5" : "10"}
+                width="20"
+                height="2"
+                rx="1"
+                fill="white"
+                transform={`rotate(${openNavigation ? "-45" : "0"})`}
+            />
+        </svg>
+    );
+};
+
+export default MenuSvg;
