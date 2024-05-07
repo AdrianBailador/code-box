271cfadad8e359d9b89e70343a10399bdcd338cd
AdrianBailador <adrianbailador@hotmail.com>
2024-05-07 16:25:52 +0100

    feat/portfolio/ProjectCard

--- NextJs/portfolio/components/ProjectCard.jsx
+++ NextJs/portfolio/components/ProjectCard.jsx
@@ -5,13 +5,15 @@ import { projects } from '../static_content/index';
 const ProjectCard = () => (
   <div className="grid grid-cols-2 gap-4">
     {projects.map((project, index) => (
-      <div key={index} className="card">
+      <div key={index} className="rounded overflow-hidden shadow-lg m-2">
         <Link href={`/project/${project.id}`}>
-          
-            <Image src={project.imageUrl} alt={project.title} width={500} height={300} />
-            <h2>{project.title}</h2>
-            <p>{project.description}</p>
-          
+         
+            <Image className="w-full" src={project.imageUrl} alt={project.title} width={500} height={300} />
+            <div className="px-6 py-4">
+              <div className="font-bold text-xl mb-2">{project.title}</div>
+              <p className="text-gray-700 text-base">{project.description}</p>
+            </div>
+        
         </Link>
       </div>
     ))}
