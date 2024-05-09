2ac304411492bcf95cf0d4feb8cb4baf0a2e75c8
Victor Alvarado <vito8916@gmail.com>
2024-05-09 16:20:24 +0100

    refactor/portfolio/adding sections component to HTML structure

--- NextJs/portfolio/app/page.js
+++ NextJs/portfolio/app/page.js
@@ -2,13 +2,24 @@ import ProfileInfo from "@/components/ProfileInfo";
 import ProfileActionButtons from "@/components/ProfileActionButtons";
 import TabsButtons from "@/components/TabsButtons";
 import ProjectCard from "@/components/ProjectCard";
+import Section from "@/components/Section";
 
 export default function Home() {
     return (
-        <main className="mt-[2rem] flex flex-col gap-4 justify-start items-center">
-            <ProfileInfo/>
-            <ProfileActionButtons/>
-            <TabsButtons/>
+        <main className="h-screen flex flex-col gap-4 justify-start items-center">
+            <div className="container lg:container mx-auto px-4">
+                <Section>
+                    <ProfileInfo/>
+                </Section>
+                <Section>
+                    <ProfileActionButtons/>
+                </Section>
+                <Section>
+                    <TabsButtons/>
+                </Section>
+
+
+            </div>
         </main>
     );
 }
