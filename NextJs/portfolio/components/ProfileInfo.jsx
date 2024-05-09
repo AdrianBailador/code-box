b25bac1b7f8270bfcd085a012ef580eeb0ff4691
Victor Alvarado <vito8916@gmail.com>
2024-05-09 17:02:18 +0100

    refactor/portfolio/styling component

--- NextJs/portfolio/components/ProfileInfo.jsx
+++ NextJs/portfolio/components/ProfileInfo.jsx
@@ -1,27 +1,24 @@
 // ProfileInfo.jsx
 import SocialIcons from './SocialIcons';
 import ProfileImage from "@/components/ProfileImage";
-import ProfileActionButtons from "@/components/ProfileActionButtons";
-
 
 const ProfileInfo = () => {
-  return (
-    <div className={'flex items-center justify-start mt-5'} >
-      <div style={{ display: 'flex', alignItems: 'center' }}>
-        <div style={{ borderRadius: '50%', overflow: 'hidden', marginRight: '1rem', border:'3px solid green', padding:'.3rem' }}>
-           <ProfileImage url={'/foto.png'} title={'My profile picture'} width={'100'} height={'100'} />
+    return (
+        <div className={'w-full flex items-center justify-start mt-5'} >
+            <div style={{ display: 'flex', alignItems: 'center' }}>
+                <div className={"border-[3px] border-primaryMain p-1.5 mr-4 rounded-full w-[125px] h-[125px]"} style={{ borderRadius: '50%', overflow: 'hidden' }}>
+                    <ProfileImage url={'/foto.png'} title={'My profile picture'} width={'120'} height={'120'} />
+                </div>
+                <div className={'profile-info flex flex-col gap-2'}>
+                    <div className="info">
+                        <h1 className={'text-2xl lg:text-3xl xl:text-4xl text-card-foreground'}>Victor Alvarado</h1>
+                        <p className={'italic text-muted-foreground'}>Software Engineer & FullStack Developer</p>
+                    </div>
+                    <SocialIcons/>
+                </div>
+            </div>
         </div>
-        <div className={'profile-info flex flex-col gap-2'}>
-          <div className="info">
-            <h1 className={'text-2xl text-gray-700 font-bold'}>Victor and Adri</h1>
-            <p className={'italic text-gray-600'}>Software Engineer & FullStack Developer</p>
-          </div>
-          <SocialIcons/>
-          
-        </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default ProfileInfo;
