4e4398a9cb9852b5578365f6319abff4a6355d10
Victor Alvarado <vito8916@gmail.com>
2024-05-10 00:24:52 +0100

    fix/portfolio/profile picture routing

--- NextJs/portfolio/components/ProfileInfo.jsx
+++ NextJs/portfolio/components/ProfileInfo.jsx
@@ -7,7 +7,7 @@ const ProfileInfo = () => {
         <div className={'w-full flex items-center justify-start mt-5'} >
             <div style={{ display: 'flex', alignItems: 'center' }}>
                 <div className={"border-[3px] border-primaryMain p-1.5 mr-4 rounded-full w-[125px] h-[125px]"} style={{ borderRadius: '50%', overflow: 'hidden' }}>
-                    <ProfileImage url={'/foto.png'} title={'My profile picture'} width={'120'} height={'120'} />
+                    <ProfileImage url={'/images/foto.png'} title={'My profile picture'} width={'120'} height={'120'} />
                 </div>
                 <div className={'profile-info flex flex-col gap-2'}>
                     <div className="info">
