d02ebadc88f8ada39d31536c6b5f8b6f658ef1c0
Victor Alvarado <vito8916@gmail.com>
2024-05-09 16:22:33 +0100

    refactor/portfolio/adding css classes

--- NextJs/portfolio/components/ProfileInfo.jsx
+++ NextJs/portfolio/components/ProfileInfo.jsx
@@ -6,15 +6,15 @@ import ProfileActionButtons from "@/components/ProfileActionButtons";
 
 const ProfileInfo = () => {
   return (
-    <div className={'flex items-center justify-center mt-5'} >
+    <div className={'flex items-center justify-start mt-5'} >
       <div style={{ display: 'flex', alignItems: 'center' }}>
-        <div style={{ borderRadius: '50%', overflow: 'hidden', marginRight: '1rem' }}>
+        <div style={{ borderRadius: '50%', overflow: 'hidden', marginRight: '1rem', border:'3px solid green', padding:'.3rem' }}>
            <ProfileImage url={'/foto.png'} title={'My profile picture'} width={'100'} height={'100'} />
         </div>
         <div className={'profile-info flex flex-col gap-2'}>
           <div className="info">
             <h1 className={'text-2xl text-gray-700 font-bold'}>Victor and Adri</h1>
-            <p className={'italic'}>FullStack Developer</p>
+            <p className={'italic text-gray-600'}>Software Engineer & FullStack Developer</p>
           </div>
           <SocialIcons/>
           
