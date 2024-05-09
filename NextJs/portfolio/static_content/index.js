efdd40978cab3ad5ac7d983468ee150004ed6534
Victor Alvarado <vito8916@gmail.com>
2024-05-09 16:58:46 +0100

    refactor/portfolio/add additional info and change icons

--- NextJs/portfolio/static_content/index.js
+++ NextJs/portfolio/static_content/index.js
@@ -1,4 +1,4 @@
-import {FaGithub, FaInstagram, FaTwitter, FaYoutube} from "react-icons/fa";
+import {RiGithubFill, RiInstagramFill, RiTwitterFill, RiYoutubeFill} from "@remixicon/react";
 
 export const navigation = [
     {
@@ -30,25 +30,25 @@ export const socialNetworks = [
         id: "0",
         name: 'Instagram',
         url: 'https://www.instagram.com/tu_usuario',
-        icon: <FaInstagram size={24}/>,
+        icon: <RiInstagramFill size={24}/>,
     },
     {
         id: "1",
         name: 'Twitter',
         url: 'https://twitter.com/tu_usuario',
-        icon: <FaTwitter size={24}/>,
+        icon: <RiTwitterFill size={24}/>,
     },
     {
         id: "2",
         name: 'YouTube',
         url: 'https://www.youtube.com/c/tu_usuario',
-        icon: <FaYoutube size={24}/>,
+        icon: <RiYoutubeFill size={24}/>,
     },
     {
         id: "3",
         name: 'GitHub',
         url: 'http://github.com',
-        icon: <FaGithub size={24}/>
+        icon: <RiGithubFill size={24}/>
     },
 ];
 
