c537b908d5ec46a4bc6a5622744fbaa51a92ad52
Victor Alvarado <vito8916@gmail.com>
2024-05-10 00:09:59 +0100

    refactor/portfolio/remove Hooks, using Server components

--- NextJs/portfolio/app/project/[slug]/page.jsx
+++ NextJs/portfolio/app/project/[slug]/page.jsx
@@ -1,33 +1,53 @@
-"use client"
-import { useEffect, useState } from 'react';
-import Image from 'next/image';
+import {notFound} from "next/navigation";
 import { projects as projectData } from '@/static_content';
+import {RiArrowLeftCircleFill, RiArrowLeftLine} from "@remixicon/react";
+import Link from "next/link";
+import Image from "next/image";
 
-const getProjectBySlug = (slug) => {
-  return projectData.find((project) => project.slug === slug);
-};
 
-const ProjectPage = ({ params }) => {
-  const { slug } = params;
-  const [project, setProject] = useState(null);
+export function generateStaticParams() {
+    return projectData.map(project => ({
+        params: { slug: project.slug },
+        props: { project }
+    }));
+}
 
-  useEffect(() => {
-    const project = getProjectBySlug(slug);
-    setProject(project);
-  }, [slug]);
 
-  if (!project) {
-    return <div>Project not found</div>;
-  }
+export default function ProjectPage({ params }) {
+    const postProject = getPostBySlug(params.slug);
 
-  return (
-    <div className="container flex min-h-screen flex-col items-center gap-10 py-24 /*lg:p-24*/ project-details">
-      <Image src={project.imageUrl} alt={project.title} width={500} height={300} />
-      <h2>{project.title}</h2>
-      <p>{project.description}</p>
-      <a href={project.githubLink} target="_blank" rel="noopener noreferrer">Ver en GitHub</a>
-    </div>
-  );
-};
+    if (!postProject) {
+        return notFound();
+    }
 
-export default ProjectPage;
+    return (
+        <div className="container flex min-h-screen flex-col items-center gap-10 py-24">
+            <Link className="hidden lg:flex rounded-full bg-primaryMain p-2" href={"/"}>
+                <RiArrowLeftCircleFill size="24px" color="white"/>
+            </Link>
+            <h1 className={"'text-2xl lg:text-3xl xl:text-4xl text-card-foreground'"}>
+                {postProject.title} {/* Cambiado de slug a title para mostrar el título */}
+            </h1>
+            <p className={"text-primaryMain"}>
+                {postProject.categories.join(', ')}
+            </p>
+            <Image
+                className={"rounded-2xl object-cover w-full h-full"}
+                width={640}
+                height={416}
+                src={postProject.imageUrl}
+                alt={`Imagen de ${postProject.title}`}
+            />
+            <p className={"text-silver-n900"}>
+                {postProject.description}
+            </p>
+            <Link className="text-card flex items-center gap-2  rounded-lg bg-primaryMain px-4 py-2" href={"/"}>
+                <RiArrowLeftLine size="20px" color="white"/> Back
+            </Link>
+        </div>
+    );
+}
+
+export function getPostBySlug(slug) {
+    return projectData.find((project) => project.slug === slug);
+}
