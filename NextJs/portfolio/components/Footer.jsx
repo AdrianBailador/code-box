2e254577059bdc52601a8aba93a385c025831947
Victor Alvarado <vito8916@gmail.com>
2024-05-10 00:23:57 +0100

    refactor/portfolio/dynamic year

--- NextJs/portfolio/components/Footer.jsx
+++ NextJs/portfolio/components/Footer.jsx
@@ -1,7 +1,9 @@
 const Footer = () => {
+    const currentYear = new Date().getFullYear();
+
     return (
         <footer className={'container md:mx-auto border-t border-silver-n60 flex flex-col justify-center items-center gap-2 py-6'}>
-            <p>© Code Box. 2022 All rights reserved</p>
+            <p>© Code Box. {currentYear} All rights reserved</p>
         </footer>
     );
 };
