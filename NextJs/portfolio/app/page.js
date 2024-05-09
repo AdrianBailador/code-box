2e3e5a0c9d9ff2380111cb2ee9b47ce9247721e8
Victor Alvarado <vito8916@gmail.com>
2024-05-09 17:00:39 +0100

    refactor/portfolio/delete unnecessary section component

--- NextJs/portfolio/app/page.js
+++ NextJs/portfolio/app/page.js
@@ -1,25 +1,13 @@
 import ProfileInfo from "@/components/ProfileInfo";
 import ProfileActionButtons from "@/components/ProfileActionButtons";
 import TabsButtons from "@/components/TabsButtons";
-import ProjectCard from "@/components/ProjectCard";
-import Section from "@/components/Section";
 
 export default function Home() {
     return (
-        <main className="h-screen flex flex-col gap-4 justify-start items-center">
-            <div className="container lg:container mx-auto px-4">
-                <Section>
-                    <ProfileInfo/>
-                </Section>
-                <Section>
-                    <ProfileActionButtons/>
-                </Section>
-                <Section>
-                    <TabsButtons/>
-                </Section>
-
-
-            </div>
+        <main className="container flex min-h-screen flex-col items-center gap-10 py-24 /*lg:p-24*/">
+            <ProfileInfo/>
+            <ProfileActionButtons/>
+            <TabsButtons/>
         </main>
     );
 }
