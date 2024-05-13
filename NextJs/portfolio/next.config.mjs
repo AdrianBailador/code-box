0db2c1dad955f58f6bc23bf764a47de48bf556bf
AdrianBailador <adrianbailador@hotmail.com>
2024-05-13 20:49:13 +0100

    ependency/portfolio/mdx

--- NextJs/portfolio/next.config.mjs
+++ NextJs/portfolio/next.config.mjs
@@ -1,4 +1,21 @@
+import createMDX from '@next/mdx'
+ // Configuración de MDX
+const withMDX = createMDX({
+    extension: /\.mdx?$/,  // Configura las extensiones .mdx y .md para ser procesadas
+    options: {
+      remarkPlugins: [],
+      rehypePlugins: [],
+      // Aquí puedes agregar cualquier configuración adicional para los plugins de remark y rehype
+    },
+  });
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  // Configure `pageExtensions` to include markdown and MDX files
+  pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
+  // Optionally, add any other Next.js config below
+}
+ 
 
-export default nextConfig;
+ 
+// Merge MDX config with Next.js config
+export default withMDX(nextConfig)
