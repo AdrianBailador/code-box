d20dc892af29929f8fef866a6ffdebc0eea13fc2
Victor Alvarado <vito8916@gmail.com>
2024-05-15 17:54:53 +0100

    refactor/portfolio/add some call to methods in API file to get all Projects

--- NextJs/portfolio/components/ProjectList.jsx
+++ NextJs/portfolio/components/ProjectList.jsx
@@ -1,11 +1,21 @@
 import ProjectCard from "@/components/ProjectCard";
-import {projects} from "@/static_content";
+import {getAllProjects} from "@/lib/api";
 
 const ProjectList = () => {
+    const allProjects = getAllProjects();
+
     return (
         <div className={"grid gap-4 grid-cols-2 mt-10"}>
-            {projects.map((project, index) => {
-                return <ProjectCard key={project.id} name={project.title} url={project.url} image={project.imageUrl} categories={project.categories} slug={project.slug}/>
+            {allProjects.map((project, index) => {
+                return <ProjectCard
+                    key={project.slug}
+                    title={project.title}
+                    coverImage={project.coverImage}
+                    excerpt={project.excerpt}
+                    /*author={project.author}*/
+                    /*date={project.date}*/
+                    slug={project.slug}
+                />
             })}
         </div>
     );
