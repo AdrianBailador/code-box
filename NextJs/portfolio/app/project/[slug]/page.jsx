6d26d194e5cc1009baf319557b2268679e0a8980
Victor Alvarado <vito8916@gmail.com>
2024-05-15 17:53:33 +0100

    feat/portfolio/add the logic to render an individual MD file

--- NextJs/portfolio/app/project/[slug]/page.jsx
+++ NextJs/portfolio/app/project/[slug]/page.jsx
@@ -1,24 +1,18 @@
 import {notFound} from "next/navigation";
-import { projects as projectData } from '@/static_content';
 import {RiArrowLeftCircleFill, RiArrowLeftLine} from "@remixicon/react";
 import Link from "next/link";
 import Image from "next/image";
+import markdownToHtml from "@/lib/markdownToHtml";
+import markdownStyles from "@/styles/markdown-styles.module.css";
+import {getProjectsBySlug} from "@/lib/api";
 
-
-export function generateStaticParams() {
-    return projectData.map(project => ({
-        params: { slug: project.slug },
-        props: { project }
-    }));
-}
-
-
-export default function ProjectPage({ params }) {
-    const postProject = getPostBySlug(params.slug);
-
-    if (!postProject) {
+export default async function ProjectPage({ params }) {
+    const post = getProjectsBySlug(params.slug);
+    if (!post) {
         return notFound();
     }
+
+    const content = await markdownToHtml(post.content || "");
 
     return (
         <div className="container flex min-h-screen flex-col items-center gap-10 py-24">
@@ -26,24 +20,28 @@ export default function ProjectPage({ params }) {
                 <RiArrowLeftCircleFill size="24px" color="white"/>
             </Link>
             <h1 className={"'text-2xl lg:text-3xl xl:text-3xl text-card-foreground'"}>
-                {postProject.title} {/* Cambiado de slug a title para mostrar el título */}
+                {post.title} {/* Cambiado de slug a title para mostrar el título */}
             </h1>
-            <p className={"text-primaryMain"}>
-                {postProject.categories.join(', ')}
-            </p>
             <Image
                 className={"rounded-2xl object-cover w-full h-full"}
                 width={640}
                 height={416}
-                src={postProject.imageUrl}
-                alt={`Imagen de ${postProject.title}`}
+                src={post.coverImage}
+                alt={`Image of ${post.title}`}
             />
-            <p className={"text-silver-n900"}>
-                {postProject.description}
-            </p>
+            <div className="max-w-2xl mx-auto">
+                <div
+                    className={markdownStyles["markdown"]}
+                    dangerouslySetInnerHTML={{__html: content}}
+                />
+            </div>
             <Link className="text-card flex items-center gap-2  rounded-lg bg-primaryMain px-4 py-2" href={"/"}>
                 <RiArrowLeftLine size="20px" color="white"/> Back
             </Link>
+            {/*<p className={"text-primaryMain"}>
+                {postProject.categories.join(', ')}
+            </p>
+            */}
         </div>
     );
 }
