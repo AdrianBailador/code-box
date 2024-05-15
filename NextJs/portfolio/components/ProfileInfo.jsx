41b422f574c04c6cf7ada853a417c2a9c74f898c
Victor Alvarado <vito8916@gmail.com>
2024-05-15 23:55:18 +0100

    refactor/portfolio/tailwind classes instead hard styles

--- NextJs/portfolio/components/ProfileInfo.jsx
+++ NextJs/portfolio/components/ProfileInfo.jsx
@@ -5,11 +5,11 @@ import ProfileImage from "@/components/ProfileImage";
 const ProfileInfo = () => {
     return (
         <div className={'w-full flex items-center justify-start mt-5'} >
-            <div style={{ display: 'flex', alignItems: 'center' }}>
-                <div className={"border-[3px] border-primaryMain p-1.5 mr-4 rounded-full w-[125px] h-[125px]"} style={{ borderRadius: '50%', overflow: 'hidden' }}>
+            <div className={"flex items-center justify-center flex-wrap gap-2"}>
+                <div className={"border-[3px] border-primaryMain p-1.5 mr-4 rounded-full w-[125px] xl:h-[125px]"} style={{ borderRadius: '50%', overflow: 'hidden' }}>
                     <ProfileImage url={'/images/foto.png'} title={'My profile picture'} width={'120'} height={'120'} />
                 </div>
-                <div className={'profile-info flex flex-col gap-2'}>
+                <div className={'profile-info flex flex-col gap-2 text-center md:text-left'}>
                     <div className="info">
                         <h1 className={'text-2xl lg:text-3xl xl:text-4xl text-card-foreground'}>Victor Alvarado</h1>
                         <p className={'italic text-muted-foreground'}>Software Engineer & FullStack Developer</p>
