b0f0264621ac041ace12dea44ebb05984ce5a589
Victor Alvarado <vito8916@gmail.com>
2024-05-15 23:55:59 +0100

    refactor/portfolio/refactoring project card to make it responsive

--- NextJs/portfolio/components/ProjectCard.jsx
+++ NextJs/portfolio/components/ProjectCard.jsx
@@ -1,32 +1,35 @@
 import Image from "next/image";
 import Link from "next/link";
 
-const ProjectCard = ({title, excerpt, coverImage, author,date, slug}) => {
+const ProjectCard = ({title, excerpt, coverImage, categories, author,date, slug}) => {
     return (
-        <Link href={`/project/${slug}`}>
-            <div className="relative flex flex-col text-gray-700 bg-clip-border rounded-xl w-full">
-                <div className="relative overflow-hidden text-gray-700 bg-white bg-clip-border rounded-xl h-52">
-                    <Image
-                        loading={"lazy"}
-                        src={coverImage}
-                        width={320}
-                        height={208}
-                        alt={"card-image"}
-                        className={"object-cover w-full h-full"}
-                    />
-                </div>
-                <div className="px-2 py-4">
-                    {/*<p className="block font-sans text-base antialiased font-medium leading-relaxed text-primaryMain">
-                        {categories}
-                    </p>*/}
-                    <div className="flex items-center justify-between mb-2">
-                        <h5 className="truncate block mb-2 font-sans text-xl antialiased font-semibold leading-snug tracking-normal text-blue-gray-900">
-                            {title}
-                        </h5>
+        <div className="w-full">
+            <Link href={`/project/${slug}`}>
+                <div className="flex flex-col text-gray-700 bg-clip-border rounded-xl">
+                    <div className="relative overflow-hidden text-gray-700 bg-white bg-clip-border rounded-xl h-52">
+                        <Image
+                            loading={"lazy"}
+                            src={coverImage}
+                            width={320}
+                            height={208}
+                            alt={"card-image"}
+                            className={"object-cover w-full h-full"}
+                        />
+                    </div>
+                    <div className="px-2 py-4">
+                        {
+                            <p className="block font-sans text-base antialiased font-medium leading-relaxed text-primaryMain">
+                                {categories.join(', ')}
+                            </p>}
+                        <div className="flex items-center justify-between mb-2">
+                            <h5 className="truncate block mb-2 font-sans text-xl antialiased font-semibold leading-snug tracking-normal text-blue-gray-900">
+                                {title}
+                            </h5>
+                        </div>
                     </div>
                 </div>
-            </div>
-        </Link>
+            </Link>
+        </div>
     );
 };
 
