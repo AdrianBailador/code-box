3d19e9179cb70cf3c0f9ac5994829282c82acdcc
Victor Alvarado <vito8916@gmail.com>
2024-05-15 23:56:24 +0100

    refactor/portfolio/refactoring project experience section

--- NextJs/portfolio/components/ProjectExperience.jsx
+++ NextJs/portfolio/components/ProjectExperience.jsx
@@ -1,84 +1,86 @@
 import Image from 'next/image';
-import { personalDescription, workExperience, education, skills, softSkills, hardSkills } from '../static_content/index';
+import {personalDescription, workExperience, education, skills, softSkills, hardSkills} from '../static_content/index';
 
 const ProjectExperience = () => (
-  <div className="relative flex flex-col gap-4 text-gray-700 bg-clip-border rounded-xl w-full mt-10 px-4">
-    <div className="description-section">
-      <p>{personalDescription.description}</p>
-    </div>
-
-    <section className='flex flex-col gap-4'>
-    <h1 className="text-2xl font-bold">Work Experience</h1>
-      {workExperience.map((experience, index) => (
-        <div key={index} className="flex flex-col gap-2">
-          <h2 className="text-xl font-bold">{experience.title}</h2>
-          <div className='flex gap-2 items-center'>
-            <span className='bg-primaryMain px-2 py-[3px] rounded-md text-xs text-white'>
-            {experience.currentJob ? "Present" : experience.startDate + " - " + experience.endDate}
-
-              </span>
-            <h3 className='text-md font-bold'>{`${experience.company} / ${experience.country}`}</h3>
-          </div>
-          <p>{experience.description}</p>
+    <div className="relative flex flex-col gap-8 text-gray-700 bg-clip-border rounded-xl w-full mt-10 px-4">
+        {/*Description or About me -- Section*/}
+        <div className="mb-4">
+            <p>{personalDescription.description}</p>
         </div>
-      ))}
-   
-    </section>
 
-    <section className='flex flex-col gap-4'>
-    <h1 className="text-2xl font-bold">Education</h1>
-    <div className="grid grid-cols-2 gap-4">
-      {education.map((experience, index) => (
-        <div key={index} className="flex flex-col gap-2">
-            <div className='flex gap-2 items-center'>
-            <span className='bg-primaryMain px-2 py-[3px] rounded-md text-xs text-white' style={{display: 'inline-block'}}>
+        {/*Work Experience -- Section*/}
+        <section className='flex flex-col gap-4'>
+            <h1 className="text-2xl font-medium">Work Experience</h1>
+            {workExperience.map((experience, index) => (
+                <div key={index} className="flex flex-col gap-2">
+                    <h2 className="text-xl font-medium">{experience.title}</h2>
+                    <div className='flex gap-2 items-center'>
+                <span className='bg-primaryMain px-2 py-[3px] rounded-md text-xs text-white'>
+                {experience.currentJob ? "Present" : experience.startDate + " - " + experience.endDate}
+
+                  </span>
+                        <h3 className='text-md font-medium'>{`${experience.company} / ${experience.country}`}</h3>
+                    </div>
+                    <p>{experience.description}</p>
+                </div>
+            ))}
+        </section>
+
+        {/*Education -- Section*/}
+        <section className='flex flex-col gap-4'>
+            <h1 className="text-2xl font-medium">Education</h1>
+            <div className="grid gap-4 grid-cols-1 md:grid-cols-2">
+                {education.map((experience, index) => (
+                    <div key={index} className="flex flex-col gap-2">
+                        <div className='flex gap-2 items-center'>
+            <span className='bg-primaryMain px-2 py-[3px] rounded-md text-xs text-white'
+                  style={{display: 'inline-block'}}>
             {experience.currentUni ? "Present" : experience.startDate + " - " + experience.endDate}
             </span>
-          </div>
-          <h2 className="text-xl font-bold">{experience.title}</h2>
-        
-          <p>{`${experience.university} / ${experience.country}`}</p>
-        </div>
-      ))}
-    </div>
-</section>
+                        </div>
+                        <h2 className="text-xl font-medium">{experience.title}</h2>
 
-<section className='flex flex-col gap-4'>
-    <h1 className="text-2xl font-bold">Hard Skills</h1>
-    <div className="grid grid-cols-4 gap-4">
-        {hardSkills.map((experience, index) => (
-            <div key={index} className="flex flex-col gap-2">
-                <div className='flex gap-2 items-center'>
-                    <img src={experience.imageUrl} alt={experience.name} className="w-10 h-10 rounded-full" />
-                </div>
-                <h2 className="text-xs font-poppins text-base leading-[9.5px] text-left text-green-600">
-    {experience.description}
-</h2>
-
-
-                <p>{experience.name}</p>
+                        <p>{`${experience.university} / ${experience.country}`}</p>
+                    </div>
+                ))}
             </div>
-        ))}
+        </section>
+
+        {/*Hard Skills -- Section*/}
+        <section className='flex flex-col gap-4'>
+            <h1 className="text-2xl font-medium">Hard Skills</h1>
+            <div className="grid grid-cols-3 md:grid-cols-4 gap-4">
+                {hardSkills.map((experience, index) => (
+                    <div key={index} className="flex flex-col gap-2">
+                        <div className='flex gap-2 items-center rounded-xl overflow-hidden'>
+                            <Image
+                                width={200}
+                                height={200}
+                                src={experience.imageUrl}
+                                alt={experience.name}
+                                className={"object-cover w-full h-full"} />
+                        </div>
+                        <h5 className="text-xs font-poppins text-base leading-[9.5px] text-left text-green-600">
+                            {experience.description}
+                        </h5>
+                        <p>{experience.name}</p>
+                    </div>
+                ))}
+            </div>
+        </section>
+
+        {/*Soft Skills -- Section*/}
+        <section className='flex flex-col gap-4'>
+            <h1 className="text-2xl font-medium">Soft Skills</h1>
+            <div className="grid grid-cols-2 gap-4">
+                {softSkills.map((experience, index) => (
+                    <div key={index} className="flex flex-col gap-2">
+                        <h5 className="text-lg font-medium">{experience.description}</h5>
+                    </div>
+                ))}
+            </div>
+        </section>
     </div>
-</section>
-
-
-<section className='flex flex-col gap-4'>
-    <h1 className="text-2xl font-bold">Soft Skills</h1>
-    <div className="grid grid-cols-2 gap-4">
-      {softSkills.map((experience, index) => (
-        <div key={index} className="flex flex-col gap-2">
-         
-          <h2 className="text-xl font-bold">{experience.description}</h2>
-        </div>
-      ))}
-    </div>
-</section>
-
-
-
-  
-  </div>
 );
 
 export default ProjectExperience;
