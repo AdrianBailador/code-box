6fa7f416b5cce30caca56e5c8e470ae2afc99327
AdrianBailador <adrianbailador@hotmail.com>
2024-05-19 18:48:10 +0100

    feat/portfolio/icon in the browser

--- NextJs/portfolio/app/layout.js
+++ NextJs/portfolio/app/layout.js
@@ -1,37 +1,42 @@
-import {Inter, Poppins} from "next/font/google";
+import { Inter, Poppins } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
-import {ThemeProvider} from "@/components/theme-provider";
+import { ThemeProvider } from "@/components/theme-provider";
 
-const inter = Inter({subsets: ["latin"]});
+const inter = Inter({ subsets: ["latin"] });
 const poppins = Poppins({
-    weight: ['300', '400', '500', '600', '700'],
-    subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700"],
+  subsets: ["latin"],
 });
 
 export const metadata = {
-    title: "Portfolio",
-    description: "Personal Portfolio",
+  title: "Portfolio",
+  description: "Personal Portfolio",
 };
 
-export default function RootLayout({children}) {
-    return (
-        <html lang="en" suppressHydrationWarning>
-        <body className={`${poppins.className} bg-background text-text`}>
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <head>
+        <link rel="icon" href="/logotemp.png" />
+        <title>{metadata.title}</title>
+        <meta name="description" content={metadata.description} />
+      </head>
+      <body className={`${poppins.className} bg-background text-text`}>
         <ThemeProvider
-            attribute="class"
-            defaultTheme="system"
-            enableSystem
-            disableTransitionOnChange
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
         >
-            <Header/>
-            <main className="container flex min-h-screen flex-col items-center gap-10 py-24 /*lg:p-24*/">
-                {children}
-            </main>
-            <Footer/>
+          <Header />
+          <main className="container flex min-h-screen flex-col items-center gap-10 py-24 /*lg:p-24*/">
+            {children}
+          </main>
+          <Footer />
         </ThemeProvider>
-        </body>
-        </html>
-    );
+      </body>
+    </html>
+  );
 }
